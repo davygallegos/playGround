0b389ab4a0b2fc678aee2d214d608e3ec0aff527
davygallegos <44485482+davygallegos@users.noreply.github.com>
2022-05-10 20:37:08 -0700

    Test 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # playGround
 all work done on Surface Duo 2
+
+Seeing If This Shows Up On Github Repo Through Remote Connection
--- playGrounds.js
+++ playGrounds.js
@@ -1,1 +1,9 @@
+function add (x,y){
+    return x + y
+}
+function substract (x,y){
+    return x - y
 
+}
+add (10,5)
+subtract (10,7) 
